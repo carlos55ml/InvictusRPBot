a34a1e4e63e0071617e9e197d2b1687ac86ebfa0
carlos55ml <cmoranlozano55@gmail.com>
2022-09-11 23:47:25 +0200

    chore: Added initial files for bot function.

---
+++ deploy-commands.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { config } = require('./config.json');
+
+const commands = [
+    /* new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+    new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+    new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'), */
+
+]
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(config.token);
+
+rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error);
---
+++ main.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const { Client, Intents, Collection, MessageEmbed, Message, MessageActionRow, MessageButton, MessageSelectMenu } = require('discord.js');
+const { config } = require('./config.json');
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS
+    ]
+});
+
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // Set a new item in the Collection
+    // With the key as the command name and the value as the exported module
+    client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) {
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction, client);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+        console.log(`[CONSOLE ==> LOG]: ${interaction.user.tag} in #${interaction.channel.name} triggered a ${interaction} at ${interaction.createdAt} .`);
+    };
+})
+
+client.on("error", (e) => console.error(e));
+client.on("warn", (e) => console.warn(e));
+client.on("debug", (e) => console.info(e));
+
+client.login(config.token)
