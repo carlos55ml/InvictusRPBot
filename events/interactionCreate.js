f297fb9f1e4f18854f35c872edecaff5a06b0371
carlos55ml <cmoranlozano55@gmail.com>
2022-09-14 22:25:07 +0200

    refactor: moved command event outside main

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,10 +1,25 @@
 const { config } = require("./../config.json")
 const { InteractionType } = require('discord.js');
+const client = require("./../main.js")
 
 module.exports = {
     name: 'interactionCreate',
     once: false,
     async execute(interaction, client) {
+        if (interaction.isCommand()) {
+            const command = client.commands.get(interaction.commandName);
+    
+            if (!command) return;
+    
+            try {
+                await command.execute(interaction, client);
+            } catch (error) {
+                console.error(error);
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+            console.log(`[CONSOLE ==> LOG]: ${interaction.user.tag} in #${interaction.channel.name} triggered a ${interaction} at ${interaction.createdAt} .`);
+        };
+
         if (interaction.isButton()) {
             if(interaction.customId === "openModal") {
                 const app = require(`./../app/accessModal.js`);
