de35ad37e062e034d356ce1db6989f935ace2a71
carlos55ml <cmoranlozano55@gmail.com>
2022-09-14 21:30:48 +0200

    feat: Added access modal handling

---
+++ app/accessModal.js
@@ -0,0 +1,54 @@
+const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+
+module.exports = {
+    async execute(interaction, client) {
+        const modal = new ModalBuilder()
+            .setCustomId(interaction.user.id)
+            .setTitle('Postulacion Acceso');
+
+        // Add components to modal
+
+        // Create the text input components
+        const nameInput = new TextInputBuilder()
+            .setCustomId('nameInput')
+            // The label is the prompt the user sees for this input
+            .setLabel("Tu nombre")
+            .setRequired(true)
+            // Short means only a single line of text
+            .setStyle(TextInputStyle.Short);
+
+        const ageInput = new TextInputBuilder()
+            .setCustomId('ageInput')
+            .setLabel("Tu edad")
+            .setRequired(true)
+            // Paragraph means multiple lines of text.
+            .setStyle(TextInputStyle.Short);
+        
+        const expInput = new TextInputBuilder()
+            .setCustomId('expInput')
+            .setLabel("Experiencia en Roleplay")
+            .setStyle(TextInputStyle.Short)
+            .setRequired(true);
+
+        const historyInput = new TextInputBuilder()
+            .setCustomId('historyInput')
+            .setLabel("Resumen de tu historia en InvictusRP")
+            .setStyle(TextInputStyle.Paragraph)
+            .setRequired(true)
+            .setMinLength(250)
+            .setMaxLength(1000);
+
+        // An action row only holds one text input,
+        // so you need one action row per text input.
+        const row1 = new ActionRowBuilder().addComponents(nameInput);
+        const row2 = new ActionRowBuilder().addComponents(ageInput);
+        const row3 = new ActionRowBuilder().addComponents(expInput);
+        const row4 = new ActionRowBuilder().addComponents(historyInput);
+
+        // Add inputs to the modal
+        modal.addComponents(row1, row2, row3, row4);
+
+        // Show the modal to the user
+        await interaction.showModal(modal);
+    },
+};
--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,11 +1,25 @@
-const config = require("../config.json")
+const { config } = require("./../config.json")
+const { InteractionType } = require('discord.js');
 
 module.exports = {
     name: 'interactionCreate',
     once: false,
     async execute(interaction, client) {
         if (interaction.isButton()) {
-            
+            if(interaction.customId === "openModal") {
+                const app = require(`./../app/accessModal.js`);
+                app.execute(interaction, client)
+            }
+        }
+
+        if (interaction.isModalSubmit()) {
+            if (interaction.type === InteractionType.ModalSubmit) {
+                if (interaction.channelId === config.accessChannelId) {
+                    const app = require(`./../app/sendModal.js`);
+                    app.execute(interaction, client)
+                    await interaction.reply({ content: 'Your submission was received successfully!', ephemeral: true });
+                }
+            }
         }
     },
 };
